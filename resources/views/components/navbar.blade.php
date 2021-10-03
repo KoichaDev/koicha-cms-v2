05b5175f71335dfecec434f9fb7512f949c88895
“Khoi <khoi@godtsagt.no>
2021-10-03 19:00:09 +0200

    Add: ✨ Basic Homepage with navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,1 +1,12 @@
-<nav></nav>
+<div class="navbar-container">
+    <nav class="navbar">
+        <img src="{{ asset('img/notes-logo.svg') }}"
+             alt="Laravel Todo App Logo"
+             width="30"
+             height="30"
+             class="navbar__image">
+        <p>Laravel Todo App</p>
+        <a href="#" class="navbar__login no-underline">Login</a>
+        <a href="#" class="no-underline">Signup</a>
+    </nav>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,10 @@
 <x-includes.head />
+<main>
+    <x-navbar />
+
+    <section class="section-container">
+        <h1 class="section-container__title">Welcome to Laravel Organize project for your Todo app!</h1>
+    </section>
+</main>
 
 <x-includes.footer />
